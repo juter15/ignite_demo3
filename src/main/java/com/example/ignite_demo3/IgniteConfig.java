ac91a5a33483687a3f4a8ed3a81a9585f4f0812a
Chan Ho <juter15@naver.com>
2022-04-19 17:30:48 +0900

    ignite 서버 노드 설정 - 최종

--- src/main/java/com/example/ignite_demo3/IgniteConfig.java
+++ src/main/java/com/example/ignite_demo3/IgniteConfig.java
@@ -1,26 +1,32 @@
 package com.example.ignite_demo3;
 
-import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteException;
-import org.apache.ignite.Ignition;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ignite.*;
 import org.apache.ignite.cache.CacheAtomicityMode;
 import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.CacheWriteSynchronizationMode;
+import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.cluster.ClusterState;
 import org.apache.ignite.configuration.*;
+import org.apache.ignite.logger.log4j.Log4JLogger;
+import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.springdata22.repository.config.EnableIgniteRepositories;
+import org.apache.logging.slf4j.SLF4JLogger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 
 @Configuration
-@EnableIgniteRepositories
+@Slf4j
+//@EnableIgniteRepositories
 public class IgniteConfig {
 
     @Bean(name = "igniteCfgInstance")
@@ -37,7 +43,8 @@ public class IgniteConfig {
         igniteConfiguration.setManagementThreadPoolSize(2);
         igniteConfiguration.setRebalanceThreadPoolSize(1);
         igniteConfiguration.setAsyncCallbackPoolSize(2);
-        //IgniteLogger log = new Slf();
+        //IgniteLogger log = new Log4JLogger();
+        //igniteConfiguration.setGridLogger(log);
         //igniteConfiguration.setGridLogger(log);
 
         //인증 활성화
@@ -46,24 +53,39 @@ public class IgniteConfig {
         // Ignite persistence configuration.
         DataStorageConfiguration storageCfg = new DataStorageConfiguration()
                 .setWalMode(WALMode.LOG_ONLY)
-                .setStoragePath("C:\\apache-ignite-2.12.0-bin\\work"+"/storage")
-                .setWalPath("C:\\apache-ignite-2.12.0-bin\\work" + "/wal")
-                .setWalArchivePath("C:\\apache-ignite-2.12.0-bin\\work" + "/wal/archive");
+//                .setStoragePath("C:\\apache-ignite-2.12.0-bin\\work"+"/storage")
+//                .setWalPath("C:\\apache-ignite-2.12.0-bin\\work" + "/wal")
+//                .setWalArchivePath("C:\\apache-ignite-2.12.0-bin\\work" + "/wal/archive");
+                .setStoragePath("/root/ignite/ignite/work"+"/storage")
+                .setWalPath("/root/ignite/ignite/work"+"/wal")
+                .setWalArchivePath("/root/ignite/ignite/work"+"/wal/archive");
 
         // Enabling the persistence.
         storageCfg.getDefaultDataRegionConfiguration().setPersistenceEnabled(true);
 
         // Applying settings.
         igniteConfiguration.setDataStorageConfiguration(storageCfg);
+        // Ignite snapShots configuration
+//        IgniteSnapshot
 
-        igniteConfiguration.setIgniteInstanceName("test-2");
+        igniteConfiguration.setSnapshotPath("/root/ignite/ignite/work"+"/snapshots");
+
 //        BinaryConfiguration binaryConfiguration = new BinaryConfiguration();
 //        binaryConfiguration.setCompactFooter(false);
 //        igniteConfiguration.setBinaryConfiguration(binaryConfiguration);
+
+        TcpDiscoveryMulticastIpFinder tcpDiscoveryMulticastIpFinder = new TcpDiscoveryMulticastIpFinder();
+
+        //tcpDiscoveryMulticastIpFinder.setAddresses(Arrays.asList("localhost","localhost:47500..47509"));
+        tcpDiscoveryMulticastIpFinder.setAddresses(Arrays.asList("192.168.0.109:47500..47509","192.168.0.111:47500..47509","192.168.0.112:47500..47509"));
+
         TcpDiscoverySpi tcpDiscoverySpi = new TcpDiscoverySpi();
-        TcpDiscoveryVmIpFinder tcpDiscoveryVmIpFinder = new TcpDiscoveryVmIpFinder();
-        tcpDiscoveryVmIpFinder.setAddresses(Collections.singletonList("127.0.0.1:47500..47509"));
-        tcpDiscoverySpi.setIpFinder(tcpDiscoveryVmIpFinder);
+
+//        TcpDiscoveryVmIpFinder tcpDiscoveryVmIpFinder = new TcpDiscoveryVmIpFinder();
+//        tcpDiscoveryVmIpFinder.setAddresses(Collections.singletonList("127.0.0.1:47500..47509"));
+
+        tcpDiscoverySpi.setIpFinder(tcpDiscoveryMulticastIpFinder);
+
         igniteConfiguration.setDiscoverySpi(tcpDiscoverySpi);
         return igniteConfiguration;
     }
@@ -71,44 +93,31 @@ public class IgniteConfig {
     @Bean(destroyMethod = "close", name="igniteInstance")
     public Ignite ignite(IgniteConfiguration igniteConfiguration) throws IgniteException {
         Ignite ignite = Ignition.start(igniteConfiguration);
-        ignite.cluster().state(ClusterState.ACTIVE);
 
+        //persistence모드는 clusterStat Active필수
+        ignite.cluster().state(ClusterState.ACTIVE);
+        //Collection<ClusterNode> nodes = ignite.cluster().forServers().nodes();
+        //log.info("nodes: {}", nodes);
+       // ignite.cluster().setBaselineTopology(nodes);
+
+        //baseline node 재조정
+        ignite.cluster().baselineAutoAdjustEnabled(true);
+        ignite.cluster().baselineAutoAdjustTimeout(1000);
+        //server Node에 캐쉬 초기화시 persistence storage 오류 발생
         // Cache configuration
-        CacheConfiguration<String, ToyModel> test1 = new CacheConfiguration<String, ToyModel>("Toy")
+/*        CacheConfiguration<String, ToyModel> test1 = new CacheConfiguration<String, ToyModel>("Toy")
                 .setCacheMode(CacheMode.PARTITIONED)
-                /* Backups=1일경우
-                *  primary 1개, Backup 1개 총 2개의 복사본ㅇ이 존재
-                * */
-                //.setBackups(1)
+                *//* Backups=1일경우
+                *  primary 1개, Backup 1개 총 2개의 복사본이 존재
+                * *//*
+                .setBackups(1)
                 .setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL)
                 .setWriteSynchronizationMode(CacheWriteSynchronizationMode.PRIMARY_SYNC)
-                .setIndexedTypes(String.class, ToyModel.class);
+                .setIndexedTypes(String.class, ToyModel.class);*/
 
-        ignite.getOrCreateCaches(Arrays.asList(test1)); //캐시 생성. 여러개 추가 가능성
+//        ignite.getOrCreateCaches(Arrays.asList(test1)); //캐시 생성. 여러개 추가 가능성
         return ignite;
     }
 
-//    @Bean
-//    public Ignite igniteInstance() {
-//        IgniteConfiguration cfg = new IgniteConfiguration();
-//
-//        // Setting some custom name for the node.
-//        cfg.setIgniteInstanceName("springDataNode");
-//
-//        // Enabling peer-class loading feature.
-//        cfg.setPeerClassLoadingEnabled(true);
-//
-//        // Defining and creating a new cache to be used by Ignite Spring Data
-//        // repository.
-//        CacheConfiguration ccfg = new CacheConfiguration("Toy ");
-//
-//        // Setting SQL schema for the cache.
-//        ccfg.setIndexedTypes(String.class, ToyModel.class);
-//
-//        cfg.setCacheConfiguration(ccfg);
-//
-//
-//
-//        return Ignition.start(cfg);
-//    }
+
 }
--- src/main/java/com/example/ignite_demo3/ToyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ignite_demo3;
-
-import org.apache.ignite.springdata22.repository.IgniteRepository;
-import org.apache.ignite.springdata22.repository.config.RepositoryConfig;
-import org.springframework.stereotype.Repository;
-
-@RepositoryConfig(cacheName = "Toy")
-public interface ToyRepository extends IgniteRepository<ToyModel, String> {
-}
